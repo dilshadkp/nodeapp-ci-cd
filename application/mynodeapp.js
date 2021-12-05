027cf8e458b3fa4fec0dab4d7ff681de64583313
Dilshad KP <dilshad.lalu@gmail.com>
2021-12-05 11:39:03 +0530

    v2

--- application/mynodeapp.js
+++ application/mynodeapp.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color: purple"><center> This is My First NodeJS Application Version 1</center><h1>');
+  res.send('<h1 style="color: purple"><center> This is My First NodeJS Application Version 2</center><h1>');
 });
 
 app.listen(PORT, HOST);
